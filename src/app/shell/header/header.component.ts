200d0910eb82ee1cb7c56c2f0ef5fa9eb33b366e
DorotaStaszczuk <dorota.staszczuk@gmail.com>
2022-05-08 20:35:41 +0200

    feat: add header component

---
+++ src/app/shell/header/header.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {
+  constructor(private authService: AuthService) {}
+
+  onLogout() {
+    this.authService.onLogout();
+  }
+}
