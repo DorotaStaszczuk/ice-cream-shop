556c5535fc21459abeec88d9d3e016a28e3105bb
DorotaStaszczuk <dorota.staszczuk@gmail.com>
2022-05-11 22:49:00 +0200

    feat: add OrdersState

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,7 +1,9 @@
 import { AuthState } from './auth';
+import { OrdersState } from './orders/orders.state';
 import { UserState } from './user/user.state';
 
 export interface AppState {
   user: UserState;
   auth: AuthState;
+  orders: OrdersState;
 }
