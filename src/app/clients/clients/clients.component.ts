54deedfe3109a9564061b8fc7a6da5abf2c1988b
DorotaStaszczuk <dorota.staszczuk@gmail.com>
2022-05-11 16:21:35 +0200

    feat: add clients component

---
+++ src/app/clients/clients/clients.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+  styleUrls: ['./clients.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ClientsComponent {
+  constructor() {}
+}
