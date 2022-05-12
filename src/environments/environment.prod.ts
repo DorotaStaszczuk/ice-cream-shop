2574293c34d085b5b1cc4089e19ed2fd56aa2db1
DorotaStaszczuk <dorota.staszczuk@gmail.com>
2022-05-12 15:58:31 +0200

    fix: add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDnMnWw0gFGDOjJ_Ck2gB89uMig_jhWKVg',
+    authDomain: 'icecreamshop-9d27b.firebaseapp.com',
+    databaseURL:
+      'https://icecreamshop-9d27b-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'icecreamshop-9d27b',
+    storageBucket: 'icecreamshop-9d27b.appspot.com',
+    messagingSenderId: '859952426284',
+    appId: '1:859952426284:web:9cae43c0595bfc20b55089',
+  },
 };
